c72644e43618dcc596155aa0a9224fdba9081a6a
Michael <michael.bitokhov@gmail.com>
2017-12-02 12:54:18 -0500

    Check answer of Day 2 Part 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,28 @@
+#include <sstream>
 #include <iostream>
+#include "includes/helpers.hpp"
 #include "includes/adventofcode.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::string input;
+    std::vector<std::vector<int>> input;
     if(argc == 1) {
-        cin >> input;
+        input = vectorize<int>(cin);
     } else {
-        input = argv[1];
+        std::stringstream args;
+        for(int i=1; i<argc; i++)
+        {
+            args << argv[i];
+        }
     }
+    // print_multi_vector(input);
 
-    cout << "Day 1: Part 1 " << day1p1(input.c_str()) << endl;
-    cout << "Day 1: Part 2 " << day1p2(input.c_str()) << endl;
+    // cout << "Day 1 Part 1 " << day1p1(input[0][0].c_str()) << endl;
+    // cout << "Day 1 Part 2 " << day1p2(input[0][0].c_str()) << endl;
 
+    cout << "Day 2 Part 1: " << day2p1(input) << endl;
 
     return 0;
 }
