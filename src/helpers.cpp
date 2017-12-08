0d3a0bc8d3c233b548cb8af861aa324b6c0cf515
Michael <michael.bitokhov@gmail.com>
2017-12-08 00:53:23 -0500

    Rework integral hashing function
    https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,12 +1,10 @@
 #include "includes/helpers.hpp"
 
 
-uint64_t integral_hash(uint64_t i)
+uint64_t integral_hash(uint64_t x)
 {
-    //The nearest prime to (2^64)/phi where phi is the golden ratio (1+sqrt(5))/2
-    //There are better ways to hash, but I only have 1-2 hours a day of
-    //research/coding and I've been stuck on this puzzle for 2 days now
-    //To be fair I haven't really spent that much time working on it
-    i *= 11400714819323198549ul;
-    return i;
+    x = (x ^ (x >> 31) ^ (x >> 62)) * UINT64_C(0x319642b2d24d8ec3);
+    x = (x ^ (x >> 27) ^ (x >> 54)) * UINT64_C(0x96de1b173f119089);
+    x = x ^ (x >> 30) ^ (x >> 60);
+    return x;
 }
