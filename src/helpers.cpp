298351dfdc70b64da663ca76bb6527d6a48f069b
Michael <michael.bitokhov@gmail.com>
2017-12-10 14:12:54 -0500

    Add credit to integral hashing function

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,8 +1,9 @@
 #include "includes/helpers.hpp"
 
-
 uint64_t integral_hash(uint64_t x)
 {
+    //With credits going to Thomas Mueller on StackOverflow
+    //https://stackoverflow.com/questions/664014
     x = (x ^ (x >> 31) ^ (x >> 62)) * UINT64_C(0x319642b2d24d8ec3);
     x = (x ^ (x >> 27) ^ (x >> 54)) * UINT64_C(0x96de1b173f119089);
     x = x ^ (x >> 30) ^ (x >> 60);
