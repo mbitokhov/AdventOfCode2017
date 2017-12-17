8598a9b3ee857eeb523a959040db1b426447b375
Michael <michael.bitokhov@gmail.com>
2017-12-17 17:07:15 -0500

    Use knuth's property again

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -2,10 +2,6 @@
 
 uint64_t integral_hash(uint64_t x)
 {
-    //With credits going to Thomas Mueller on StackOverflow
-    //https://stackoverflow.com/questions/664014
-    x = (x ^ (x >> 31) ^ (x >> 62)) * UINT64_C(0x319642b2d24d8ec3);
-    x = (x ^ (x >> 27) ^ (x >> 54)) * UINT64_C(0x96de1b173f119089);
-    x = x ^ (x >> 30) ^ (x >> 60);
+    x *= 0x9e3779b97f4a7c15;
     return x;
 }
