de1365f053058850a80959ea2f31b8526e00dc6f
Michael <michael.bitokhov@gmail.com>
2017-12-24 18:43:31 -0500

    Implement FNV hash for arithmetic values of std::vector

--- includes/helpers.hpp
+++ includes/helpers.hpp
@@ -4,8 +4,56 @@
 #include <vector>
 #include <sstream>
 #include <string>
+#include <type_traits>
+#include <limits.h>
 
 uint64_t integral_hash(uint64_t);
+
+namespace std {
+
+    template <typename dtype>
+    struct hash<std::vector<dtype> >
+    {
+        template <typename T = dtype,
+                  typename = std::enable_if_t<std::is_arithmetic<T>::value> >
+
+        size_t operator()(const std::vector<T> &input) const
+        {
+            // Use different constants for different values sizeof(size_t)
+            const auto FNV_prime = [](){
+                constexpr int type = sizeof(size_t) * CHAR_BIT;
+                static_assert(type <= 64, "FNV > 64 bits is not implemented");
+
+                if(type <= 32)
+                {
+                    return (size_t) 16777619ul;
+                } else {
+                    return (size_t) 1099511628211ul;
+                }
+            }();
+            const auto FNV_offset = [](){
+                constexpr int type = sizeof(size_t) * CHAR_BIT;
+                static_assert(type <= 64, "FNV > 64 bits is not implemented");
+
+                if(type <= 32)
+                {
+                    return (size_t) 2166136261ul;
+                } else {
+                    return (size_t) 14695981039346656037ul;
+                }
+            }();
+
+            // Perform hash
+            size_t hashed = FNV_offset;
+            for(const auto &n:input)
+            {
+                hashed ^= n;
+                hashed *= FNV_prime;
+            }
+            return hashed;
+        }
+    };
+}
 
 template <typename dtype>
 std::vector<dtype> vectorize(std::istream &input)
